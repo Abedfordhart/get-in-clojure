80bc9aa304d31042192f0dc4bfb9bf70e815b618
Andrew Bedford-Hart <abedfordhart@gmail.com>
2018-02-22 15:12:52 -0800

    Add tests for each type of associative structure

--- getIn.js
+++ getIn.js
@@ -1,21 +1,46 @@
-const user = {
+const map = {
   username: 'Sally',
   profile: {
     name: 'Sally Clojurian', address: {
       city: 'Austin', state: 'TX'
     }
   }
-}
+};
+
+const vector = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]];
+
+const mixed = {
+  username: 'Jimmy',
+  pets: [{
+    name: 'Rex',
+    type: 'dog'
+  },
+  {
+    name: 'Sniffles',
+    type: 'Hamster'
+  }]
+};
 
 const getIn = (obj, ks, notFound = null) => {
   for (let i = 0; i < ks.length; i++) {
-    if (obj == null) {
-      return notFound;
-    }
     obj = obj[ks[i]];
   }
   return obj === undefined ? notFound : obj
 }
 
-console.log(getIn(user, ['profile', 'name', 'pet'], 'Item not found!'));
+//Map
+console.log('---Map---');
+console.log(getIn(map, ['profile', 'name']));
+console.log(getIn(map, ['profile', 'address', 'city']));
+console.log(getIn(map, ['profile', 'address', 'zip-code']));
+console.log(getIn(map, ['profile', 'address', 'zip-code'], 'no zip code!'));
+//Vector
+console.log('---Vector---');
+console.log(getIn(vector, [0,2]));
+console.log(getIn(vector, [2,1]));
+//Mixed
+console.log('---Mixed---');
+console.log(getIn(mixed, ['pets', 1, 'type']));
 
